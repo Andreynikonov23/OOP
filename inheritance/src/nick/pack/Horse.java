6ac941f97c7091b00b961560cf8df300d0322899
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-03-09 11:41:07 +0300

    Add files via upload

---
+++ inheritance/src/nick/pack/Horse.java
@@ -0,0 +1,8 @@
+package nick.pack;
+
+public class Horse {
+    public final String HEAD = "Голова";
+    public final String BODY = "Тело";
+    public final String TAIL = "Хвост";
+    public final String HOOVES = "Копыта";
+}
