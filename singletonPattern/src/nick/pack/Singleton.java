853e870c14707b2488cd031257898bc060d5018a
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-04-01 22:06:12 +0300

    Add files via upload

---
+++ singletonPattern/src/nick/pack/Singleton.java
@@ -0,0 +1,15 @@
+package nick.pack;
+
+public class Singleton {
+    private static Singleton instance;
+
+
+    private Singleton(){
+    }
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
